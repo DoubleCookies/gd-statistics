644d689280fa86bb3f5b5e4638dce47e543eabe5
DoubleCookies <12849336+DoubleCookies@users.noreply.github.com>
2022-06-07 10:24:05 +0300

    Packages update, fix translation

--- web/src/i18n.js
+++ web/src/i18n.js
@@ -6,31 +6,29 @@ const resources = {
     en: {
         translation: {
             aboutTitle: "What is it?",
-            aboutText: "I collect data about top-50 most downloaded and most liked levels in Geometry Dash " +
-                "(overall and for \"demon\" difficulty levels). After that I form table and " +
-                "post it on Geometry Dash Wiki (ru). At the same time I create data for two templates which are used " +
-                "in articles about most downloaded levels. " +
-                "In result there is semi-automatic articles and templates update (and as a bonus, Wiki contains " +
-                "changes for these pages).",
+            aboutText: "This project is about collecting data about levels in Geometry Dash — how many times level was downloaded, liked, " +
+                " who is creator, which soundtrack it uses, etc. After that different tables are filled with data.. and that's all!",
             locationTitle: "Where can I find it?",
-            locationText: "You can check all data <1>here</1>.",
+            locationText: "You can check data <1>here</1>.",
 
             workTitle: "How does it work?",
             workList: "Everything is pretty simple and consecutive:<1>" +
-                "<0>Fetch data from Geometry Dash servers and get page (10 levels). Depending on type " +
-                "(downloads/likes) form two levels lists.</0>" +
-                "<0>Stop processing pages when we have 50 demon levels (automatically " +
-                "we will have 50 levels with every difficulty).</0>" +
+                "<0>Fetch data from Geometry Dash servers and get page (10 featured levels). We are getting data until we found " +
+                "last featured level.</0>" +
+                "<0>Form list for epic levels (because every epic level is featured we won't miss anything)</0>" +
                 "<0>Both lists split for another two lists  — for all levels and for demon-only.</0>" +
-                "<0>Transform every list in txt format " +
-                "(with nuances like template types, presence or absence of links, dividers, etc.)</0>" +
-                "<0>Process text for templates and save it.</0>" +
+                "<0>By manipulating the lists, we form various new lists - by difficulty, by soundtracks usage, by " +
+                "amount of levels from creator, etc.</0>" +
+                "<0>Save data in different files.</0>" +
                 "<0><2>Done!</2></0>" +
                 "</1>",
             plansTitle: "Any plans?",
-            plansList: "<0><1>I can check old commits, collect data from old months/years and " +
+            plansList: "<0>" +
+                "<1>Fix data fetching.</1>" +
+                "<1>Maybe check old commits, collect data from old months/years and " +
                 "build graph/table/<2>other thing</2>.</1>" +
-                "<1>Add illustrations. Everybody likes illustrations I guess?</1></0>",
+                "<1>Add illustrations. Everybody likes illustrations I guess?</1>" +
+                "</0>",
         }
     },
     ru: {
@@ -42,29 +40,32 @@ const resources = {
                 "таблицы.. и всё!",
 
             locationTitle: "Где можно найти?",
-            locationText: "Посмотреть на все таблицы можно <1>тут</1>.",
+            locationText: "Посмотреть на данные можно <1>тут</1>.",
 
             workTitle: "Как он работает?",
             workList: "Всё довольно просто и последовательно:<1>" +
                 "<0>Обращаемся к серверам Geometry Dash и получаем страницу (10 featured-уровней). После этого " +
                 "продолжаем получать страницы с уровнями до тех пор, пока они не закончатся.</0>" +
                 "<0>На основе списка featured-уровней формируем отдельный список epic-уровней (так как они " +
-                "автоматически являются featured — список будет полным).</0>" +
-                "<0>Всячески манипулируя списками формируем различные новые списки — по сложностям, по кол-ву " +
+                "автоматически являются featured — мы ничего не упустим).</0>" +
+                "<0>Манипулируя списками формируем различные новые списки — по сложностям, по кол-ву " +
                 "использований саундтреков, по кол-ву уровней у определенных игроков, и т. д.</0>" +
                 "<0>Сохраняем списки в отдельные файлы.</0>" +
                 "<0><2>Готово!</2></0>" +
                 "</1>",
             plansTitle: "Есть ли планы на будущее?",
-            plansList: "<0><1>Можно будет пройтись по старым коммитам, собрать информацию по прошлым месяцам/годам " +
+            plansList: "<0>" +
+                "<1>Поправить сбор данных.</1>" +
+                "<1>Можно будет пройтись по старым коммитам, собрать информацию по прошлым месяцам/годам " +
                 "и построить сводную табличку/график/<2>другую штуку</2>.</1>" +
-                "<1>Добавить иллюстраций. Все же любят картинки?</1></0>",
+                "<1>Добавить иллюстраций. Все же любят картинки?</1>" +
+                "</0>",
         }
     }
 }
 
 i18n.use(detector).use(initReactI18next).init({
-    fallbackLng: 'ru',
+    fallbackLng: 'en',
     resources,
 })
 
